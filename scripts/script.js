6aaf6e17a19a0150b7722e4bdf9393ec57446bf2
Semen Bushmanov <bush.sem@mail.ru>
2025-02-16 22:04:12 +0500

    Add dark mode interaction

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,19 @@ const sidebarBtn = document.querySelector('.open-btn');
 const wrapper = document.querySelector('.wrapper');
 const navLinkText = document.querySelectorAll('.nav-link__text');
 const sidebarBtnText = document.querySelectorAll('.sidebar__btn-text');
-
-
+const darkThemeBtn = document.querySelector('.dark-theme-btn');
+const darkModeText = darkThemeBtn.querySelector('.dark-mode');
+const lightModeText = darkThemeBtn.querySelector('.light-mode');
 
 sidebarBtn.addEventListener('click', () => {
   wrapper.classList.toggle('sidebar-opened');
   navLinkText.forEach(e => e.classList.toggle('visually-hidden'));
   sidebarBtnText.forEach(e => e.classList.toggle('visually-hidden'));
+});
+
+darkThemeBtn.addEventListener('click', () => {
+  wrapper.classList.toggle('dark');
+  darkThemeBtn.classList.toggle('dark'); 
+  darkModeText.classList.toggle('visually-hidden');
+  lightModeText.classList.toggle('visually-hidden');
 });
