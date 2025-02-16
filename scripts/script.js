59b25e5cfd5d99f95db825ea88e870365a47d598
Semen Bushmanov <bush.sem@mail.ru>
2025-02-17 00:02:57 +0500

    Add styling for dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const sidebarBtn = document.querySelector('.open-btn');
 const wrapper = document.querySelector('.wrapper');
 const navLinkText = document.querySelectorAll('.nav-link__text');
+const navSearchText = document.querySelector('.nav-search__text');
 const sidebarBtnText = document.querySelectorAll('.sidebar__btn-text');
 const darkThemeBtn = document.querySelector('.dark-theme-btn');
 const darkModeText = darkThemeBtn.querySelector('.dark-mode');
@@ -9,6 +10,7 @@ const lightModeText = darkThemeBtn.querySelector('.light-mode');
 sidebarBtn.addEventListener('click', () => {
   wrapper.classList.toggle('sidebar-opened');
   navLinkText.forEach(e => e.classList.toggle('visually-hidden'));
+  navSearchText.classList.toggle('visually-hidden');
   sidebarBtnText.forEach(e => e.classList.toggle('visually-hidden'));
 });
 
